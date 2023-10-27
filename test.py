707eacfca157d349891c5a07f3786441863b7b5a
Ruslan Klimchuk <klimatwua@gmail.com>
2023-10-27 23:21:15 +0300

    change for conflict

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-import datetime
-
-name = input ("What's your name? ")
-print("Hello! ", name,"!")
 print("We learn git!!!")
 print("We Learn GitHub!")
 print("New Line!!!")
-print("Today: ", datetime.today().strftime('%d/%m/%Y'))
+
