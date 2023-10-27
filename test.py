0e7b52688aaac292e42aee6d96b0846eb5c79899
Ruslan Klimchuk <klimatwua@gmail.com>
2023-10-27 23:08:45 +0300

    refactoring code

--- test.py
+++ test.py
@@ -4,8 +4,5 @@ name = input ("What's your name? ")
 print("Hello! ", name,"!")
 print("We learn git!!!")
 print("We Learn GitHub!")
-<<<<<<< HEAD
 print("New Line!!!")
-=======
 print("Today: ", datetime.today().strftime('%d/%m/%Y'))
->>>>>>> 5e8a75bb2de0a261ee2c0919c24777dc305a48a7
