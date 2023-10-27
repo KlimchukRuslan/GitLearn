7c80b21f9f35ecb6c6a3b7549387b7159f8f854b
Ruslan Klimchuk <klimatwua@gmail.com>
2023-10-27 22:29:27 +0300

    Add new line

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+import datetime
+
 name = input ("What's your name? ")
 print("Hello! ", name,"!")
 print("We learn git!!!")
 print("We Learn GitHub!")
+<<<<<<< HEAD
 print("New Line!!!")
+=======
+print("Today: ", datetime.today().strftime('%d/%m/%Y'))
+>>>>>>> 5e8a75bb2de0a261ee2c0919c24777dc305a48a7
